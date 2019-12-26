17fe92d80a9f0cf39e1fd870da3ea0883d70a805
lavanyak2020 <lavanyakcse2020@gmail.com>
2019-12-26 20:46:16 +0530

    [lavanya] solved tictactoe

---
+++ classes/GameLauncher.java
@@ -0,0 +1,8 @@
+import com.tictactoe.*;
+
+public class GameLauncher{
+	public static void main(String[] args) {
+		TicTacToe ttt = new TicTacToe();
+		ttt.startGame();
+	}
+}
---
+++ src/com/tictactoe/Player.java
@@ -0,0 +1,11 @@
+package com.tictactoe;
+
+public class Player{
+	private String name;
+	public Player(String name){
+		this.name = name;
+	}
+	String getName(){
+		return name;
+	}
+}
